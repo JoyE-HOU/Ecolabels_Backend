cd5bd367f2fc1f555ed34d810cc21a083248be23
Joy Evans <joyevans@me.com>
2021-06-02 11:54:04 -0500

    added more validations

--- app/models/ecolabel.rb
+++ app/models/ecolabel.rb
@@ -2,4 +2,10 @@ class Ecolabel < ApplicationRecord
     has_many :categories
 
     validates :name, :description, :owner_category, :level, :party_verification, :criteria, :accepted_elsewhere, presence: true 
+
+    YES = 1
+    NO = 2
+    MAYBE = 3
+
+    enum format: {yes: YES, no:NO, maybe:MAYBE}
 end
