ebde0d84394728673db4f64a40be7883e1308943
Joy Evans <joyevans@me.com>
2021-06-03 10:05:44 -0500

    Reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_162102) do
+ActiveRecord::Schema.define(version: 2021_06_03_142808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.bigint "ecolabel_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["ecolabel_id"], name: "index_categories_on_ecolabel_id"
+  end
+
+  create_table "ecolabel_categories", force: :cascade do |t|
+    t.bigint "ecolabel_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_ecolabel_categories_on_category_id"
+    t.index ["ecolabel_id"], name: "index_ecolabel_categories_on_ecolabel_id"
   end
 
   create_table "ecolabels", force: :cascade do |t|
@@ -38,5 +45,6 @@ ActiveRecord::Schema.define(version: 2021_06_01_162102) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "categories", "ecolabels"
+  add_foreign_key "ecolabel_categories", "categories"
+  add_foreign_key "ecolabel_categories", "ecolabels"
 end
