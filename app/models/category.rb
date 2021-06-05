9b8db3c29ae7aeb37cda1b1be8da5b2b40c50c31
Joy Evans <joyevans@me.com>
2021-06-05 18:29:48 -0500

    updated models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :ecolabel_categories
-  has_many :ecolabels, :through :ecolabel_categories
+  has_many :ecolabels, through: :ecolabel_categories
   
   validates :name, presence: true
 end
