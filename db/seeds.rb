c5f86a9a8af1237a78075be142c884d54d5809dc
Joy Evans <joyevans@me.com>
2021-06-30 19:13:07 -0500

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -886,6 +886,78 @@ Ecolabel.create(
     website:"https://sustainable.golf/"
     accepted_elsewhere:"yes"
     )
+Ecolabel.create(
+    name:"Global Good Agricultural Practice (GAP)", 
+    description:"Global G.A.P. is a private sector body that sets voluntary standards for the certification of agricultural products around the globe.", 
+    owner_category:"industry association", 
+    level:"n/a", 
+    year_established:1997, 
+    party_verification:"third", 
+    criteria:"n/a", 
+    price:"n/a",
+    website:"https://www.globalgap.org/uk_en/"
+    accepted_elsewhere:"yes"
+    )
+Ecolabel.create(
+    name:"Global GreenTag Certified", 
+    description:"Global Green Tag® is a third party, green product rating and certification system, underpinned by scientific and Life Cycle Assessment (LCA) processes. ", 
+    owner_category:"for-profit", 
+    level:"n/a", 
+    year_established:2010, 
+    party_verification:"third", 
+    criteria:"n/a", 
+    price:"n/a",
+    website:"https://www.globalgreentag.com/"
+    accepted_elsewhere:"yes"
+    )
+Ecolabel.create(
+    name:"Global Organic Textile Standard", 
+    description:"The Global Organic Textile Standard (GOTS) was developed with the aim to unify the various existing standards and draft standards in the field of eco textile processing and to define world-wide recognised requirements that ensure organic status of textiles, from harvesting of the raw materials, through environmentally and socially responsible manufacturing up to labelling in order to provide a credible assurance to the end consumer.", 
+    owner_category:"other", 
+    level:"n/a", 
+    year_established:2006, 
+    party_verification:"third", 
+    criteria:"n/a", 
+    price:"n/a",
+    website:"https://global-standard.org/"
+    accepted_elsewhere:"yes"
+    )
+Ecolabel.create(
+    name:"Global Recycle Standard", 
+    description:"The Global Recycled Standard is intended for companies that are making and/or selling products with recycled content.", 
+    owner_category:"non-profit", 
+    level:"n/a", 
+    year_established:2008, 
+    party_verification:"second", 
+    criteria:"n/a", 
+    price:"n/a",
+    website:"https://textileexchange.org/documents/grs-4-0-rcs-2-0-consultation-feedback-summary/"
+    accepted_elsewhere:"no"
+    )
+Ecolabel.create(
+    name:"Gold Standard", 
+    description:"The Gold Standard distinguishes the highest quality carbon offset projects in the voluntary and compliance environmental markets and is a key policy tool for the NGO community to influence the development of the rapidly growing global carbon markets.", 
+    owner_category:"non-profit", 
+    level:"n/a", 
+    year_established:2003, 
+    party_verification:"third", 
+    criteria:"n/a", 
+    price:"n/a",
+    website:"https://www.goldstandard.org/"
+    accepted_elsewhere:"yes"
+    )
+Ecolabel.create(
+    name:"GoodWeave", 
+    description:"GoodWeave is working to end child labor in the carpet industry and to offer educational opportunities to children in South Asia.", 
+    owner_category:"non-profit", 
+    level:"n/a", 
+    year_established:1994, 
+    party_verification:"second", 
+    criteria:"n/a", 
+    price:"n/a",
+    website:"https://goodweave.org/"
+    accepted_elsewhere:"yes"
+    )
 
 
 Category.create(name: "appliances")
@@ -894,6 +966,7 @@ Category.create(name: "bedding")
 Category.create(name: "building products")
 Category.create(name: "buildings")
 Category.create(name: "carbon")
+Category.create(name: "carbon offsets")
 Category.create(name: "campsite services")
 Category.create(name: "cleaning products")
 Category.create(name: "commercial and industrial buildings")
@@ -911,6 +984,7 @@ Category.create(name: "forest products/paper")
 Category.create(name: "furniture")
 Category.create(name: "health care services & equipment")
 Category.create(name: "homes")
+Category.create(name: "land holdings")
 Category.create(name: "light duty vehicles")
 Category.create(name: "lumber")
 Category.create(name: "machinery & equipment")
